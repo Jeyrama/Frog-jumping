4590d45efc79d9be908642e1e618b2fa8dd4d7e4
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-02-13 09:40:43 -0800

    Update Frog-jumping.js

--- Frog-jumping.js
+++ Frog-jumping.js
@@ -20,3 +20,18 @@ Example:
 
 
 // Solutions
+
+function solution(a) {
+	let c = 0, i = 0;
+
+	while (typeof a[i] !== 'undefined') {
+    c++;
+    i += a[i];
+    
+    if (c > a.length) {
+    	return -1;
+    }
+  }
+
+  return a.length ? c : -1;
+}
